29c909fcffad55f1a67c40e6a9bb19d97cb570e2
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-05 12:17:37 -0700

    Added an example.

---
+++ examples/example.d
@@ -0,0 +1,24 @@
+
+
+
+int main() {
+	import std.stdio : stdout, stderr;
+	import WeatherForecast : getForecast, WeatherData;
+
+	getForecast(delegate(WeatherData weather_data, Exception err) {
+		if (err) {
+			stderr.writefln("%s", err);
+		} else {
+			stdout.writefln("latitude: %s", weather_data.latitude);
+			stdout.writefln("longitude: %s", weather_data.longitude);
+			stdout.writefln("city: %s", weather_data.city);
+			stdout.writefln("region: %s", weather_data.region);
+			stdout.writefln("country: %s", weather_data.country);
+			stdout.writefln("postal: %s", weather_data.postal);
+			stdout.writefln("temperature: %s", weather_data.temperature);
+			stdout.writefln("summary: %s", weather_data.summary);
+		}
+	});
+
+	return 0;
+}
